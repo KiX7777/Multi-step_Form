7d9987033d23750e5f4c84dc164f151223d51bfb
KiX7777 <kristianboi202@gmail.com>
2023-02-17 20:18:38 +0100

    steps object

--- script.js
+++ script.js
@@ -4,10 +4,20 @@ const stepNums = document.querySelectorAll('.stepNum');
 const mainConts = document.querySelectorAll('.main');
 const timeToggle = document.getElementById('planTime');
 const monthly = document.getElementById('monthly');
+const yearly = document.querySelector('.yearly');
 const plans = document.querySelectorAll('.planOption');
 const prices = document.querySelectorAll('.planPrice');
 const free = document.querySelectorAll('.free');
 const addons = document.querySelectorAll('.addOnOption');
+const changePlan = document.querySelector('.changePlan');
+
+const next2 = document.getElementById('next2');
+const next3 = document.getElementById('next3');
+
+let state = {
+  time: 'Monthly',
+  selectedAddons: [],
+};
 
 stepNums.forEach((num) => {
   num.addEventListener('click', () => {
@@ -25,6 +35,7 @@ stepNums.forEach((num) => {
   });
 });
 
+// activeTime;
 console.log(timeToggle.checked);
 timeToggle.addEventListener('change', () => {
   timeToggle.checked
@@ -45,10 +56,16 @@ timeToggle.addEventListener('change', () => {
     free.forEach((txt) => {
       txt.style.display = 'block';
     });
+    monthly.classList.remove('activeTime');
+    yearly.classList.add('activeTime');
+    state.time = 'Yearly';
   } else {
     free.forEach((txt) => {
       txt.style.display = 'none';
     });
+    monthly.classList.add('activeTime');
+    yearly.classList.remove('activeTime');
+    state.time = 'Monthly';
   }
 });
 
@@ -66,11 +83,57 @@ addons.forEach((option) => {
       check.checked = true;
       check.disabled = false;
       option.classList.add('activePlan');
+      option.classList.add('selectedAddon');
     } else {
       check.checked = false;
       check.disabled = true;
 
       option.classList.remove('activePlan');
+      option.classList.remove('selectedAddon');
     }
   });
+});
+
+changePlan.addEventListener('click', () => {
+  mainConts.forEach((cont) => {
+    cont.style.display = 'none';
+  });
+  mainConts[2].style.display = 'flex';
+  mainConts[2].style.animation = 'container 700ms ease 0s 1 normal forwards';
+  stepNums.forEach((step) => step.classList.remove('activeNum'));
+  stepNums[2].classList.add('activeNum');
+});
+
+function nextStep() {
+  let selectedPlan = document.querySelector('.activePlan');
+  let selectedAddons = document.querySelectorAll('.selectedAddon');
+  let selectedPlanName = selectedPlan.querySelector('.planTitle').textContent;
+  let planPrice = selectedPlan.querySelector('.planPrice').textContent;
+  state.planPrice = planPrice;
+  state.selectedPlan = selectedPlanName;
+  selectedAddons.forEach((add) => {
+    let addonName = add.querySelector('h3').textContent;
+    let addonPrice = add.querySelector('.addOnPrice').textContent;
+
+    if (!state.selectedAddons.includes(addonName)) {
+      //if the addon is not selected log it to the state, otherwise skip it
+      let addon = {
+        addonName: addonName,
+        addonPrice: addonPrice,
+      };
+
+      state.selectedAddons.push(addon);
+    }
+  });
+}
+
+next2.addEventListener('click', () => {
+  nextStep();
+  console.log(state);
+});
+
+next3.addEventListener('click', () => {
+  state.selectedAddons = [];
+  nextStep();
+  console.log(...state.selectedAddons);
 });
