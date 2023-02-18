018b1864da4ba06af8e48a809241d1fb8a3b1d83
KiX7777 <kristianboi202@gmail.com>
2023-02-18 19:02:08 +0100

    switch steps

--- script.js
+++ script.js
@@ -10,7 +10,12 @@ const prices = document.querySelectorAll('.planPrice');
 const free = document.querySelectorAll('.free');
 const addons = document.querySelectorAll('.addOnOption');
 const changePlan = document.querySelector('.changePlan');
-
+const addonsSummary = document.querySelector('.addonsSummary');
+const chosenPlan = document.querySelector('.chosenPlan');
+const chosenPrice = document.querySelector('.chosenPrice');
+const nextBtns = document.querySelectorAll('.nextBtn');
+const gobackBtns = document.querySelectorAll('.gobackBtn');
+const next1 = document.getElementById('next1');
 const next2 = document.getElementById('next2');
 const next3 = document.getElementById('next3');
 
@@ -27,11 +32,13 @@ stepNums.forEach((num) => {
     num.classList.toggle('activeNum');
     mainConts.forEach((cont) => {
       cont.style.display = 'none';
+      cont.classList.remove('activeStep');
     });
     let contID = num.textContent - 1;
     mainConts[contID].style.display = 'flex';
     mainConts[contID].style.animation =
       'container 700ms ease 0s 1 normal forwards';
+    mainConts[contID].classList.add('activeStep');
   });
 });
 
@@ -105,12 +112,21 @@ changePlan.addEventListener('click', () => {
 });
 
 function nextStep() {
+  addonsSummary.innerHTML = ``;
+  state.selectedAddons = [];
   let selectedPlan = document.querySelector('.activePlan');
   let selectedAddons = document.querySelectorAll('.selectedAddon');
   let selectedPlanName = selectedPlan.querySelector('.planTitle').textContent;
   let planPrice = selectedPlan.querySelector('.planPrice').textContent;
   state.planPrice = planPrice;
   state.selectedPlan = selectedPlanName;
+  let chosenPlanName = chosenPlan.querySelector('h3');
+  chosenPlanName.textContent = `${state.selectedPlan}(${state.time})`;
+  // chosenPlan.style.fontSize = '1.4rem';
+  // chosenPlan.style.fontWeight = '700';
+  // chosenPlan.style.color = '#02295a';
+
+  chosenPrice.textContent = state.planPrice;
   selectedAddons.forEach((add) => {
     let addonName = add.querySelector('h3').textContent;
     let addonPrice = add.querySelector('.addOnPrice').textContent;
@@ -121,9 +137,14 @@ function nextStep() {
         addonName: addonName,
         addonPrice: addonPrice,
       };
-
       state.selectedAddons.push(addon);
     }
+
+    addonsSummary.innerHTML += ` 
+    <div class="chosenAddon">
+      <p class="chosenAddonName">${addonName}</p>
+      <p class="chosenAddonPrice">${addonPrice}</p>
+  </div>`;
   });
 }
 
@@ -133,7 +154,55 @@ next2.addEventListener('click', () => {
 });
 
 next3.addEventListener('click', () => {
-  state.selectedAddons = [];
   nextStep();
   console.log(...state.selectedAddons);
+});
+
+function switchtoNextStep() {
+  let num = document.querySelector('.activeNum');
+  let contID = num.textContent;
+  mainConts.forEach((cont) => {
+    cont.style.display = 'none';
+    cont.classList.remove('activeStep');
+  });
+  mainConts[contID].style.display = 'flex';
+  mainConts[contID].style.animation =
+    'container 700ms ease 0s 1 normal forwards';
+  mainConts[contID].classList.add('activeStep');
+  console.log(contID);
+  stepNums.forEach((num) => {
+    num.classList.remove('activeNum');
+  });
+  // num.classList.toggle('activeNum');
+  stepNums[contID].classList.add('activeNum');
+}
+
+// next1.addEventListener('click', switchtoNextStep);
+// next2.addEventListener('click', switchtoNextStep);
+
+nextBtns.forEach((btn) => {
+  btn.addEventListener('click', switchtoNextStep);
+});
+
+function goBack() {
+  let num = document.querySelector('.activeNum');
+  let contID = num.textContent - 1;
+  mainConts.forEach((cont) => {
+    cont.style.display = 'none';
+    cont.classList.remove('activeStep');
+  });
+  mainConts[contID - 1].style.display = 'flex';
+  mainConts[contID - 1].style.animation =
+    'container 700ms ease 0s 1 normal forwards';
+  mainConts[contID - 1].classList.add('activeStep');
+  console.log(contID);
+  stepNums.forEach((num) => {
+    num.classList.remove('activeNum');
+  });
+  // num.classList.toggle('activeNum');
+  stepNums[contID - 1].classList.add('activeNum');
+}
+
+gobackBtns.forEach((btn) => {
+  btn.addEventListener('click', goBack);
 });
